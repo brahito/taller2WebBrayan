da02b8d65966fa0f8cc541a0cc784c5418d0be1b
brayan <brayantino24@hotmail.com>
2019-05-10 02:25:29 -0500

    avance web

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://localhost:40000';
+const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'tienda';
@@ -16,62 +16,80 @@ const client = new MongoClient(url);
 var db = null;
 
 // Use connect method to connect to the Server
-    client.connect(function(err) {
-      assert.equal(null, err);
+client.connect(function(err) {
+    assert.equal(null, err);
+    
+    db = client.db(dbName);
+    
+    //client.close();
+});
 
-      console.log("Connected successfully to server");
-    
-      const db = client.db(dbName);
-    
-      client.close();
-    });
 
-    var app = express();
+var app = express();
 
-    app.use(express.static('public'));
-    app.use(express.urlencoded({ extended: true }));
-    
-    app.engine('handlebars', exphbs());
-    app.set('view engine', 'handlebars');
-    
-    app.get('/', function(request, response){
-        var contexto = {
-            titulo: 'Página principal',
-        };
-        response.render('tienda', contexto);
-    });
-    
-app.get('/tienda/:categoria?',function(request,response){
-    
-    console.log(request.params.categoria);
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-    var query ={};
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(request, response){
+    var contexto = {
+        titulo: 'Página principal',
+    };
+    response.render('tienda', contexto);
+});
+
+app.get('/tienda/:categoria?', function(request, response){
+    
+    console.log(request.query.precio);
+
+    var query = {};
     if(request.params.categoria){
         query.categoria = request.params.categoria;
     }
     if(request.query.precio){
-        query.precio = { $lte: request.query.precio}
+        query.precio = { $lte: request.query.precio };
     }
 
     var collection = db.collection('productos');
-
-    collection.find(query).toArray(function(err,docs){
-        assert.equal(err,null);
-
-        var contexto ={
+    // Find some documents
+    collection.find(query)
+        .toArray(function(err, docs) {
+        assert.equal(err, null);
+        
+        var contexto = {
             productos: docs,
             categoria: request.params.categoria,
-            precio:request.query.precio,
-            esHardware: request.params.categoria == "Hardware",
-            esSoftware: request.params.categoria == "Software",
+            precio: request.query.precio,
+            esSoftware: request.params.categoria == "Hardware",
+            esHardware: request.params.categoria == "Software",
+            esAccesorio: request.params.categoria == "Accesorio",
         };
         response.render('tienda', contexto);
     });
+    
 });
 
-app.post('/login', function(request,response){
-    console.log(request.body);
+app.get('/tienda/producto/:nombre', function(req, res){
 
+    var collection = db.collection('productos');
+    collection.find({ nombre: req.params.nombre })
+        .toArray(function(err, docs){
+            console.log(docs);
+
+            var contexto = {
+                producto: docs[0]
+            };
+            res.render('producto', contexto);
+        });
+});
+
+app.post('/login', function(request, response){
+    // crear un archivo con la información del usuario
+    console.log(request.body);
+    // redireccionar a otra página
     response.redirect('/bienvenida');
 });
+
 app.listen(3000);
